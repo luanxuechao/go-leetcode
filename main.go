41832634994ac30418a32916855b91d1870de92a
chao <luanxuechaowd@gmail.com>
2019-10-22 21:24:16 +0800

    feat:daily commit

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
-	var result [][]int = medium.FourSum([]int{-3, -2, -1, 0, 0, 1, 2, 3}, 0)
+	// var result [][]int = medium.FourSum([]int{-3, -2, -1, 0, 0, 1, 2, 3}, 0)
+	// fmt.Println(result)
+	var result int = medium.FourSumCount([]int{1, 2}, []int{-2, -1}, []int{-1, 2}, []int{0, 2})
 	fmt.Println(result)
 }
